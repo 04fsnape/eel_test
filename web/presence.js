4ff0d5331f652b93ba88febfc3714fe5b68ea8b8
Finn <33752988+04fsnape@users.noreply.github.com>
2018-06-19 19:33:24 +0100

    Delete presence.js

--- web/presence.js
+++
@@ -1,8 +0,0 @@
-function presenceCheck() {
-  var inputs = document.getElementsByClassName("inputs");
-  for (var i = 0; i < inputs.length; i++) {
-    if inputs[i].value.length == 0 {
-      inputs[i].style.borderBottom = "1px solid red !important";
-    }
-}
-}
